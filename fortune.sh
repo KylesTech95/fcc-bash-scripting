57d2dfd11e42a6a29ce7d79dad60b0842924f3aa
KylesTech95 <108553476+KylesTech95@users.noreply.github.com>
2023-12-01 06:47:47 -0500

    Save progress: 1450.1

--- fortune.sh
+++ fortune.sh
@@ -2,7 +2,6 @@
 # Program to tell a persons fortune
 echo -e "\n~~ Fortune Teller ~~\n"
 RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
-echo ${RESPONSES[5]}
 N=$(( RANDOM % 6 ))
 GET_FORTUNE(){
 if [[ ! $1 ]]
